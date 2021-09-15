7a28dd9329447a565eb19d7586148fa354648c49
SerhiiZadorozhnyi <71183966+SerhiiZadorozhnyi@users.noreply.github.com>
2021-09-15 20:59:25 +0300

    Update fetch-countries-api.js

--- src/js/fetch-countries-api.js
+++ src/js/fetch-countries-api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 axios.defaults.baseURL = 'https://restcountries.eu/rest/v2/name';
 
 export const fetchCountries = (searchQuery) => {
-        return axios
-            .get(`/${searchQuery}`)
-            .then(response => response.data);
+    return axios
+        .get(`/${searchQuery}`)
+        .then(response => response.data);
 }
